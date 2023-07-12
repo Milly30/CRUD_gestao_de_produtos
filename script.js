22cd54830ff58afb1feb1aa52bed5e96fcb9a53b
Milly30 <108283759+Milly30@users.noreply.github.com>
2023-07-12 00:28:33 -0300

    .

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const API_BASE_URL = 'https://649a1d4a79fbe9bcf8404b5a.mockapi.io/users/20201214
 
 const productForm = document.getElementById('productForm');
 const productTableBody = document.getElementById('productTableBody');
+const cancelButton = document.getElementById('cancelButton');
+
+let isEditing = false;
+let editingProductId = null;
 
 function buscarProdutos() {
   fetch(API_BASE_URL)
@@ -21,6 +25,7 @@ function buscarProdutos() {
 
 function criarLinhaTabela(produto) {
   const row = document.createElement('tr');
+  row.setAttribute('data-product-id', produto.id);
 
   const idCell = document.createElement('td');
   idCell.textContent = produto.id;
@@ -51,6 +56,14 @@ function criarLinhaTabela(produto) {
   row.appendChild(imagemCell);
 
   const acoesCell = document.createElement('td');
+  const editarButton = document.createElement('button');
+  editarButton.textContent = 'Editar';
+  editarButton.classList.add('editar-button');
+  editarButton.addEventListener('click', () => {
+    editarProduto(produto.id);
+  });
+  acoesCell.appendChild(editarButton);
+
   const excluirButton = document.createElement('button');
   excluirButton.textContent = 'Excluir';
   excluirButton.classList.add('excluir-button');
@@ -63,7 +76,24 @@ function criarLinhaTabela(produto) {
   return row;
 }
 
-function criarProduto(dadosProduto) {
+function preencherFormulario(produto) {
+  document.getElementById('productId').value = produto.id;
+  document.getElementById('nameInput').value = produto.nome;
+  document.getElementById('categoryInput').value = produto.categoria;
+  document.getElementById('priceInput').value = produto.preco;
+  document.getElementById('descriptionInput').value = produto.descricao;
+  document.getElementById('imageInput').value = '';
+}
+
+function salvarProduto(dadosProduto) {
+  if (isEditing) {
+    editarProdutoAPI(dadosProduto);
+  } else {
+    criarProdutoAPI(dadosProduto);
+  }
+}
+
+function criarProdutoAPI(dadosProduto) {
   fetch(API_BASE_URL, {
     method: 'POST',
     headers: {
@@ -82,6 +112,25 @@ function criarProduto(dadosProduto) {
     });
 }
 
+function editarProdutoAPI(dadosProduto) {
+  fetch(`${API_BASE_URL}/${editingProductId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dadosProduto),
+  })
+    .then(response => response.json())
+    .then(produtoAtualizado => {
+      const linhaAtualizada = document.querySelector(`[data-product-id="${editingProductId}"]`);
+      linhaAtualizada.replaceWith(criarLinhaTabela(produtoAtualizado));
+      limparFormulario();
+    })
+    .catch(error => {
+      console.error('Erro ao editar o produto:', error);
+    });
+}
+
 function excluirProduto(idProduto) {
   fetch(`${API_BASE_URL}/${idProduto}`, {
     method: 'DELETE',
@@ -95,28 +144,43 @@ function excluirProduto(idProduto) {
     });
 }
 
-function limparFormulario() {
-  productForm.reset();
+function editarProduto(idProduto) {
+  fetch(`${API_BASE_URL}/${idProduto}`)
+    .then(response => response.json())
+    .then(produto => {
+      preencherFormulario(produto);
+      isEditing = true;
+      editingProductId = idProduto;
+    })
+    .catch(error => {
+      console.error('Erro ao buscar o produto para edição:', error);
+    });
 }
+
+cancelButton.addEventListener('click', () => {
+  limparFormulario();
+});
 
 productForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  const nomeInput = document.getElementById('nameInput');
-  const categoriaInput = document.getElementById('categoryInput');
-  const precoInput = document.getElementById('priceInput');
-  const descricaoInput = document.getElementById('descriptionInput');
-  const imagemInput = document.getElementById('imageInput');
+  const productIdInput = document.getElementById('productId');
+  const nameInput = document.getElementById('nameInput');
+  const categoryInput = document.getElementById('categoryInput');
+  const priceInput = document.getElementById('priceInput');
+  const descriptionInput = document.getElementById('descriptionInput');
+  const imageInput = document.getElementById('imageInput');
 
   const dadosProduto = {
-    nome: nomeInput.value,
-    categoria: categoriaInput.value,
-    preco: parseFloat(precoInput.value),
-    descricao: descricaoInput.value,
-    imagem: URL.createObjectURL(imagemInput.files[0]),
+    id: productIdInput.value,
+    nome: nameInput.value,
+    categoria: categoryInput.value,
+    preco: parseFloat(priceInput.value),
+    descricao: descriptionInput.value,
+    imagem: URL.createObjectURL(imageInput.files[0]),
   };
 
-  criarProduto(dadosProduto);
+  salvarProduto(dadosProduto);
 });
 
 buscarProdutos();
